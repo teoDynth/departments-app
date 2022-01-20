dec61fa0207a87e45704bd95358dd4103edac6ce
Oleh <true_khan@outlook.com>
2022-01-20 13:21:04 +0200

    setup.py update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='departments-app',
+    version='1.0',
+    packages=['rest', 'forms', 'tests', 'views', 'models', 'service'],
+    url='https://departments-app-2022.herokuapp.com/',
+    license='',
+    author='true_khan',
+    author_email='true_khan@outlook.com',
+    description='Departments App Final Project'
+)
